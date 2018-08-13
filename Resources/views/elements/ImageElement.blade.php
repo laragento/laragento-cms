0c6dc32e768e3d024dc1f252591b49cdff148362
tanja.braether <tanja.braether@mexan.ch>
2018-08-13 11:13:29 +0200

    Implement ImageElement

--- Resources/views/elements/ImageElement.blade.php
+++ Resources/views/elements/ImageElement.blade.php
@@ -1,1 +1,62 @@
-<h1>image</h1>
+<h2>{{ $element->title }}</h2>
+
+<div>
+    <label>{{ trans('cms::element_image.image') }}
+        <?php $contentField = $element->fieldByTitle('path');
+        $fieldValue = !empty($block) ? $element->valueByField($contentField, $block)->value : null;
+        ?>
+        <input type="file" name="fields[{{$element->id}}][{{$contentField->id}}]"
+               value="{{ old("fields[$element->id][$contentField->id]") ? old("fields[$element->id][$contentField->id]") : null }}">
+        @if ($errors->has("fields[$element->id][$contentField->id]"))
+            <span class="form-error is-visible">{{ $errors->first("fields[$element->id][$contentField->id]") }}</span>
+        @endif
+    </label>
+</div>
+<div>
+    <label>{{ trans('cms::element_image.name') }}
+        <?php $contentField = $element->fieldByTitle('name');
+        $fieldValue = !empty($block) ? $element->valueByField($contentField, $block)->value : null;
+        ?>
+        <input type="text" name="fields[{{$element->id}}][{{$contentField->id}}]" autofocus placeholder=""
+               value="{{ old("fields[$element->id][$contentField->id]") ? old("fields[$element->id][$contentField->id]") :  $fieldValue ? $fieldValue : '' }}">
+        @if ($errors->has("fields[$element->id][$contentField->id]"))
+            <span class="form-error is-visible">{{ $errors->first("fields[$element->id][$contentField->id]") }}</span>
+        @endif
+    </label>
+</div>
+<div>
+    <label>{{ trans('cms::element_image.alt') }}
+        <?php $contentField = $element->fieldByTitle('alt');
+        $fieldValue = !empty($block) ? $element->valueByField($contentField, $block)->value : null;
+        ?>
+        <input type="text" name="fields[{{$element->id}}][{{$contentField->id}}]" autofocus placeholder=""
+               value="{{ old("fields[$element->id][$contentField->id]") ? old("fields[$element->id][$contentField->id]") :  $fieldValue ? $fieldValue : '' }}">
+        @if ($errors->has("fields[$element->id][$contentField->id]"))
+            <span class="form-error is-visible">{{ $errors->first("fields[$element->id][$contentField->id]") }}</span>
+        @endif
+    </label>
+</div>
+<div>
+    <label>{{ trans('cms::element_image.width') }}
+        <?php $contentField = $element->fieldByTitle('width');
+        $fieldValue = !empty($block) ? $element->valueByField($contentField, $block)->value : null;
+        ?>
+        <input type="number" min="0" name="fields[{{$element->id}}][{{$contentField->id}}]" autofocus placeholder=""
+               value="{{ old("fields[$element->id][$contentField->id]") ? old("fields[$element->id][$contentField->id]") :  $fieldValue ? $fieldValue : '' }}">
+        @if ($errors->has("fields[$element->id][$contentField->id]"))
+            <span class="form-error is-visible">{{ $errors->first("fields[$element->id][$contentField->id]") }}</span>
+        @endif
+    </label>
+</div>
+<div>
+    <label>{{ trans('cms::element_image.height') }}
+        <?php $contentField = $element->fieldByTitle('height');
+        $fieldValue = !empty($block) ? $element->valueByField($contentField, $block)->value : null;
+        ?>
+        <input type="number" min="0" name="fields[{{$element->id}}][{{$contentField->id}}]" autofocus placeholder=""
+               value="{{ old("fields[$element->id][$contentField->id]") ? old("fields[$element->id][$contentField->id]") :  $fieldValue ? $fieldValue : '' }}">
+        @if ($errors->has("fields[$element->id][$contentField->id]"))
+            <span class="form-error is-visible">{{ $errors->first("fields[$element->id][$contentField->id]") }}</span>
+        @endif
+    </label>
+</div>
--- Resources/views/elements/frontend/ImageElement.blade.php
+++ Resources/views/elements/frontend/ImageElement.blade.php
@@ -1,1 +1,11 @@
-<h1>image</h1>
+<div>
+    <?php
+    $srcValue = $element->valueByField( $element->fieldByTitle('path'), $block)->value;
+    $nameValue = $element->valueByField( $element->fieldByTitle('name'), $block)->value;
+    $altValue = $element->valueByField( $element->fieldByTitle('alt'), $block)->value;
+    $widthValue = $element->valueByField( $element->fieldByTitle('width'), $block)->value;
+    $heightValue = $element->valueByField( $element->fieldByTitle('height'), $block)->value;
+    @if($srcValue)
+        <img src="{{$srcValue}}" alt="{{$altValue}}" name="{{$nameValue}}" width="{{$widthValue}}" height="{{$heightValue}}"/>
+    @endif
+</div>
